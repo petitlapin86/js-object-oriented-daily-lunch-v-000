93bd551678094b0b52b6184cb3a0d1f6512f14b7
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-17 13:39:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,21 @@ meals(){ //returns all meals customer had delivered
       return delivery.meal();
     });
   }
-  
+
 deliveries() //returns all deliveries a customer has had
 totalSpent(){
     return this.meals().reduce(function(sum, meal) {
       return sum + meal.price;
     }, 0);
   } //returns total amount spent as a function of the cost of meals delivered
+}
 
+let mealId = 0  // begins at zero
+class Meal { //defines a class of meal
+  constructor(name, price) {
+  this.name = name;
+  this.price = price;
+  this.id = ++mealId; //counts adding one each new id number
+  store.meals.push(this); //pushes this info to store array
+}
 }
